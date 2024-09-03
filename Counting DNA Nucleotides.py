94c619f1f5455e569436acdcdca40a257f1f9b10
mniszka <aflisnik@wp.pl>
2024-09-03 15:22:02 +0200

    Create Counting DNA Nucleotides.py

---
+++ Counting DNA Nucleotides.py
@@ -0,0 +1,18 @@
+# count how many nucleotides (A, T, C, G) are in the DNA sequence
+
+def count_nucleotides(dna_sequence):
+  A = dna_sequence.count("A")
+  T = dna_sequence.count("T")
+  C = dna_sequence.count("C")
+  G = dna_sequence.count("G")
+  
+  return A, T, C, G
+
+
+
+if __name__ == "__main__":
+  with open('rosalind.txt','r') as file:
+    dna_sequence = file.read().strip()
+
+  A, T, C, G = count_nucleotides(dna_sequence)
+  print(f"A: {A}, T: {T}, C: {C}, G: {G}")
